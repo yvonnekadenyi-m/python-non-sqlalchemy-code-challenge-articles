6fba55d60b8ba29f7387adbe8f12736c14b3a7a8
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-11-18 22:36:13 +0300

    added class magazine

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -91,6 +91,12 @@ class Magazine:
     class Article:
     all = []
 
+    def __init__(self, author, magazine, title):
+        self.author = author
+        self.magazine = magazine
+        self.title = title
+        Article.all.append(self)
+
 
 
 
