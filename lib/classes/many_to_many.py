313185d658909f76833ae8154bae920cccd57ef7
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-11-18 22:21:16 +0300

    added class magazine

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -74,7 +74,13 @@ class Magazine:
         return [article for article in Article.all if article.magazine == self]
 
 
+    def contributors(self):
+        return list({article.author for article in self.articles()})
 
+    def article_titles(self):
+        titles = [article.title for article in self.articles()]
+        return titles if titles else None
+ 
 
     def contributors(self):
         pass
